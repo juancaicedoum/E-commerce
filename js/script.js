8fbfd5d435f9a46d505c6dc13faea5ffeb24748b
Juan Caicedo <70857895+juancaicedoum@users.noreply.github.com>
2025-03-21 20:02:45 -0500

    HTML: Creamos un formulario con 3 campos (nombre, tel y correo) y el JS tiene la funcionalidad del formulario

--- js/script.js
+++ js/script.js
@@ -1,152 +1,43 @@
-(function($) {
+//Capturar la información del formulario
+const formulario = document.querySelector("form")
 
-  "use strict";
+//Agregar un evento al enviar el formulario
+formulario.addEventListener("submit", function(evento){
+  evento.preventDefault();
+  let valido = true; //Variable para validar el formulario
+  const nombre = document.getElementById("nombre");
+  const email = document.getElementById("email");
+  const telefono = document.getElementById("telefono");
 
-  var initPreloader = function() {
-    $(document).ready(function($) {
-    var Body = $('body');
-        Body.addClass('preloader-site');
-    });
-    $(window).load(function() {
-        $('.preloader-wrapper').fadeOut();
-        $('body').removeClass('preloader-site');
-    });
+  //Validar nombre
+  if (nombre.value.trim() === ""){
+    nombre.classList.add("invalid-feedback"); //Agregar una clase de bootstrap a la alerta.
+    valido = false;
+    } else {
+      nombre.classList.remove("invalid-feedback"); //Remover la clase de bootstrap a la alerta.
+    }
+
+  //Validar email
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (!emailRegex.test(email.value.trim())){
+    email.classList.add("invalid-feedback"); //Agregar una clase de bootstrap a la alerta.
+    valido = false;
+  } else {
+    email.classList.remove("invalid-feedback"); //Remover la clase de bootstrap a la alerta.
   }
 
-  // init Chocolat light box
-	var initChocolat = function() {
-		Chocolat(document.querySelectorAll('.image-link'), {
-		  imageSize: 'contain',
-		  loop: true,
-		})
-	}
-
-  var initSwiper = function() {
-
-    var swiper = new Swiper(".main-swiper", {
-      speed: 500,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-
-    var category_swiper = new Swiper(".category-carousel", {
-      slidesPerView: 6,
-      spaceBetween: 30,
-      speed: 500,
-      navigation: {
-        nextEl: ".category-carousel-next",
-        prevEl: ".category-carousel-prev",
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        991: {
-          slidesPerView: 4,
-        },
-        1500: {
-          slidesPerView: 6,
-        },
-      }
-    });
-
-    var brand_swiper = new Swiper(".brand-carousel", {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      speed: 500,
-      navigation: {
-        nextEl: ".brand-carousel-next",
-        prevEl: ".brand-carousel-prev",
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        991: {
-          slidesPerView: 3,
-        },
-        1500: {
-          slidesPerView: 4,
-        },
-      }
-    });
-
-    var products_swiper = new Swiper(".products-carousel", {
-      slidesPerView: 5,
-      spaceBetween: 30,
-      speed: 500,
-      navigation: {
-        nextEl: ".products-carousel-next",
-        prevEl: ".products-carousel-prev",
-      },
-      breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        991: {
-          slidesPerView: 4,
-        },
-        1500: {
-          slidesPerView: 6,
-        },
-      }
-    });
+  //Validar teléfono
+  const telefonoRegex = /^[0-9]{10}$/;
+  if (!telefonoRegex.test(telefono.value.trim())){
+    telefono.classList.add("invalid-feedback"); //Agregar una clase de bootstrap a la alerta.
+    valido = false;
+  } else {
+    telefono.classList.remove("invalid-feedback"); //Remover la clase de bootstrap a la alerta.
   }
 
-  var initProductQty = function(){
-
-    $('.product-qty').each(function(){
-
-      var $el_product = $(this);
-      var quantity = 0;
-
-      $el_product.find('.quantity-right-plus').click(function(e){
-          e.preventDefault();
-          var quantity = parseInt($el_product.find('#quantity').val());
-          $el_product.find('#quantity').val(quantity + 1);
-      });
-
-      $el_product.find('.quantity-left-minus').click(function(e){
-          e.preventDefault();
-          var quantity = parseInt($el_product.find('#quantity').val());
-          if(quantity>0){
-            $el_product.find('#quantity').val(quantity - 1);
-          }
-      });
-
-    });
-
+  //Si el formulario es válido, enviar la información
+  if (valido){
+    alert("¡Formulario enviado correctamente!");
+    formulario.reset(); //Limpiar el formulario
   }
-
-  // init jarallax parallax
-  var initJarallax = function() {
-    jarallax(document.querySelectorAll(".jarallax"));
-
-    jarallax(document.querySelectorAll(".jarallax-keep-img"), {
-      keepImg: true,
-    });
-  }
-
-  // document ready
-  $(document).ready(function() {
-    
-    initPreloader();
-    initSwiper();
-    initProductQty();
-    initJarallax();
-    initChocolat();
-
-  }); // End of a document
-
-})(jQuery);
+});
